6d17c231135970a84274d61843f0feb7bbf914ba
Coke_And_1ce <22373442@buaa.edu.cn>
2025-11-08 12:47:17 +0800

    goal

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@
 # 自动处理所有分片 TFRecord 文件
 # ==========================================
 
-DATA_DIR=/home/linyihan/linyh/datasets/modified_libero_rlds/libero_10_no_noops/1.0.0
+DATA_DIR=/home/linyihan/linyh/datasets/modified_libero_rlds/libero_object_no_noops/1.0.0
 export CUDA_VISIBLE_DEVICES=1
 
-for FILE in $DATA_DIR/liber_o10-train.tfrecord-*; do
+for FILE in $DATA_DIR/libero_object-train.tfrecord-*; do
   echo "🚀 开始处理: $FILE"
   python latent.py "$FILE"
   echo "✅ 完成: $FILE"
